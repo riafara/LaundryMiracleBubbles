deb1a92b774e2e80ca567405215b1b456981f1b5
sindisukma <117698581+sindisukma@users.noreply.github.com>
2022-12-10 09:15:29 +0700

    Update paket.php

--- paket.php
+++ paket.php
@@ -5,19 +5,19 @@ require ("koneksi.php");
 
 <html>
 <head>
-        <title>Home</title>
+        <title>Paket</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
     <body>
-        <h1>Daftar Paket Laundry</h1>
+        <h1>Tabel Paket</h1>
         <!-- <h1>Selamat Datang <?php echo $email;?></h1> -->
         <table border='2' style="text-align:center">
             <tr>
                 <td>No</td>
-                <td>ID Paket</td>
-                <td>Nama Paket</td>
-                <td>Harga /Kg</td>
+                <td>Paket</td>
+                <td>Harga/Kg</td>
                 <td>Deskripsi</td>
+                
                 <td colspan="2"></td>
             </tr>
             <?php
@@ -28,7 +28,6 @@ require ("koneksi.php");
             
 
             while ($row = mysqli_fetch_array($result)){
-                $id = $row['id_paket'];
                 $paket = $row['paket'];
                 $harga = $row['harga_kilo'];
                 $des = $row['deskripsi'];
@@ -36,19 +35,21 @@ require ("koneksi.php");
             ?>
             <tr>
                 <td><?php echo $no; ?></td>
-                <td><?php echo $id; ?></td>
                 <td><?php echo $paket; ?></td>
                 <td><?php echo $harga; ?></td>
                 <td><?php echo $des; ?></td>
+                
                 <td>
-                    <a href='editpaket.php?id_paket=$row[id_paket]'>Edit</a>
-                    <a href='hapuspaket.php?id_paket =$row[id_paket]'>Hapus</a>
-                </td>
+                <a href="editpaket.php?id_customer=<?php echo $row['id_paket']; ?>">edit</a>
+                <a href="hapuspaket.php?id_customer=<?php echo $row['id_paket']; ?>">hapus</a>
+
+            </td>
+                
             </tr>        
             <?php
             $no++;   
             }?>
         </table>  
-        <p><a href="logout.php">Log Out</a></p>
+        <a href="login.php">Log Out</a>
     </body>
 </html>
