aba61f3df5227336c8ce7345129be7c1fd5d71b4
sindisukma <117698581+sindisukma@users.noreply.github.com>
2022-12-10 08:18:19 +0700

    Create transaksi.php

---
+++ transaksi.php
@@ -0,0 +1,67 @@
+<?php
+require ("koneksi.php");
+// $email = $_GET['user_fullname'];
+?>
+
+<html>
+<head>
+        <title>Home</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+    <body>
+        <h1>Transaksi</h1>
+        <!-- <h1>Selamat Datang <?php echo $email;?></h1> -->
+        <table border='2' style="text-align:center">
+            <tr>
+                <td>No</td>
+                <td>ID Transaksi</td>
+                <td>Tanggal</td>
+                <td>Customer</td>
+                <td>Nama Paket</td>
+                <td>Qty</td>
+                <td>Harga</td>
+                <td>Bayar</td>
+                <td>Kembali</td>
+                <td colspan="2"></td>
+            </tr>
+            <?php
+            $query  = "SELECT * FROM tb_transaksi";
+            $result = mysqli_query($koneksi, $query);
+            $no     = 1;
+
+            
+
+            while ($row = mysqli_fetch_array($result)){
+                $id = $row['id_transaksi'];
+                $tgl = $row['tanggal'];
+                $cus = $row['id_customer'];
+                $paket = $row['id_paket'];
+                $qty = $row['qty'];
+                $harga = $row['biaya'];
+                $bayar = $row['bayar'];
+                $kembali = $row['kembalian'];
+                
+            ?>
+            <tr>
+                <td><?php echo $no; ?></td>
+                <td><?php echo $id; ?></td>
+                <td><?php echo $tgl; ?></td>
+                <td><?php echo $cus; ?></td>
+                <td><?php echo $paket; ?></td>
+                <td><?php echo $qty; ?></td>
+                <td><?php echo $harga; ?></td>
+                <td><?php echo $bayar; ?></td>
+                <td><?php echo $kembali; ?></td>
+                <td>
+                    <a href='edit.php?id_user=$row[id_user]'>Edit</a>
+                    <a href='hapustransaksi.php?id_transaksi =$row[id_transaksi]'>Hapus</a>
+                </td>
+                
+            </tr>        
+            <?php
+            $no++;   
+            }?>
+        </table>  
+        <a href="home.php">Log Out</a>
+    </body>
+</html>
