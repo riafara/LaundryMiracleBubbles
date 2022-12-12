d2902ce2ca13b9e85de2bc53a7adc94c1f035bc6
sindisukma <117698581+sindisukma@users.noreply.github.com>
2022-12-12 08:56:06 +0700

    Create hapustransaksi.php

---
+++ hapustransaksi.php
@@ -0,0 +1,7 @@
+<?php
+require ('koneksi.php');
+$id = $_GET['id_transaksi'];
+mysqli_query($koneksi, "DELETE FROM tb_transaksi WHERE id_transaksi ='$id'")or die(mysqli_errno($koneksi));
+
+header("location:transaksi.php");
+?>
