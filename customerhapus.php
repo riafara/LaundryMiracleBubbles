d6bebb6109772f58d1c63668b3d90c77f13ba2bd
riafara <riafaradhila30@gmail.com>
2022-12-12 10:19:08 +0700

    Update customerhapus.php

--- customerhapus.php
+++ customerhapus.php
@@ -3,5 +3,5 @@ require ('koneksi.php');
 $id = $_GET['id_customer'];
 mysqli_query($koneksi, "DELETE FROM tb_customer WHERE id_customer ='$id'")or die(mysqli_errno($koneksi));
 
-header("location:customer.php");
+header("location:tcustomer.php");
 ?>
