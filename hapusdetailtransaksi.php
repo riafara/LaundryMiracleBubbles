bc1cbaca11c9e617b56ca62bb959ae0581020650
riafara <riafaradhila30@gmail.com>
2022-12-12 10:16:42 +0700

    Update hapusdetailtransaksi.php

--- hapusdetailtransaksi.php
+++ hapusdetailtransaksi.php
@@ -2,5 +2,5 @@
 require('koneksi.php');
 $id = $_GET['id_detail'];
 mysqli_query($koneksi, "DELETE FROM tb_detail_transaksi WHERE id_detail='$id'") or die(mysql_error());
-header("location: detailtransaksi.php")
+header("location: tdttransaksi.php")
 ?>
