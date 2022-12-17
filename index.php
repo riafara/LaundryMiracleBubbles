afe647d416e4e406112068acc69da14eca3f7241
riafara <riafaradhila30@gmail.com>
2022-12-17 19:05:28 +0700

    update

--- index.php
+++ index.php
@@ -61,75 +61,47 @@ $sesLvl = $_SESSION['level'];
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
+            <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
                 <a class="nav-link" href="tables.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>User</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                TABEL DATA 
-            </div>
-
-            <!-- Nav Item - Pages Collapse Menu -->
+                    <span>Manajemen User</span></a>
+            </li><!-- Nav Item - Dashboard -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Data Master</span>
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Transaksi</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">MASTER</h6>
-                        <a class="collapse-item" href="tables.php">User</a>
-                        <a class="collapse-item" href="tables.php">Customer</a>
-                        <a class="collapse-item" href="tables.php">Paket</a>
-                        <a class="collapse-item" href="tables.php">Transaksi</a>
-                        <a class="collapse-item" href="tables.php">Detail Transaksi</a>
+                       
+                        <a class="collapse-item" href="ttransaksi.php">Transaksi</a>
+                        <a class="collapse-item" href="tdttransaksi.php">Detail Transaksi</a>
+                       
                     </div>
                 </div>
                
             </li>
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                            Tables
-                        </div>
-             <!-- Nav Item - Tables -->
-             <li class="nav-item">
-                <a class="nav-link" href="tables.php">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Data User</span></a>
-            </li>     
-             <!-- Nav Item - Tables -->
-             <li class="nav-item">
-                <a class="nav-link" href="tcustomer.php">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Data Customer</span></a>
-            </li>  
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
+            <li class="nav-item active">
                 <a class="nav-link" href="tpaket.php">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Data Paket</span></a>
-            </li> 
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="ttransaksi.php">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Data Transaksi</span></a>
-            </li>  
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tdttransaksi.php">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Data Detail Transaksi</span></a>
-            </li>             
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Paket Laundry</span></a>
+            </li>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="tcustomer.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Customer</span></a>
+            </li>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Laporan</span></a>
+            </li>
+            
+
 
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
