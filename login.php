64b74dc412308412eeecb069be034c3ea9b8d6eb
Firdachs08 <91067475+Firdachs08@users.noreply.github.com>
2022-12-25 21:11:17 +0700

    login.php

--- login.php
+++ login.php
@@ -90,11 +90,11 @@ if (isset($_POST['submit'])) {
 
             <div class="col-xl-10 col-lg-12 col-md-9">
 
-                <div class="card o-hidden border-0 shadow-lg" style="margin-top: 125px;">
+                <div class="card o-hidden border-0 shadow-lg" style="margin-top: 100px;">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6 d-none d-lg-block"><img src="img/logo4.jpg"></div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
