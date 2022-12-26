18684af4f6aa8f9d24189b2204d2b30352989b11
riafara <riafaradhila30@gmail.com>
2022-12-26 13:46:45 +0700

    Update hapuspaket.php

--- hapuspaket.php
+++ hapuspaket.php
@@ -3,5 +3,5 @@ require ('koneksi.php');
 $id = $_GET['id_paket'];
 mysqli_query($koneksi, "DELETE FROM tb_paket WHERE id_paket ='$id'")or die(mysqli_errno($koneksi));
 
-header("location:paket.php");
+header("location:tpaket.php");
 ?>
