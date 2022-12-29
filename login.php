bfa274cc792501e63ac5acb2f7f1ff6509bf00a4
Firdachs08 <91067475+Firdachs08@users.noreply.github.com>
2022-12-29 12:11:59 +0700

    login.php

--- login.php
+++ login.php
@@ -94,7 +94,11 @@ if (isset($_POST['submit'])) {
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                        hhh
+                        <div class="col-lg-5 d-none d-lg-block"><img src="img/logoo.jpg" style="width: 500px"></div>
+                            <div class="col-lg-7">
+                                <div class="p-5">
+                                    <div class="text-center"><br>
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
                                     <form class="user" action="login.php" method="post">
                                         <div class="form-group">
