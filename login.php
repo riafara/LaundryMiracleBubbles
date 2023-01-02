6ca7fea4cffa187cbf0ec6ca7a9a668ec8cbcff6
sindisukma <117698581+sindisukma@users.noreply.github.com>
2023-01-02 20:05:38 +0700

    Update lagi

--- login.php
+++ login.php
@@ -98,7 +98,7 @@ if (isset($_POST['submit'])) {
                             <div class="col-lg-7">
                                 <div class="p-5">
                                     <div class="text-center"><br>
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back</h1>
+                                        <h1 class="h4 text-gray-900 mb-4"></h1>
                                     </div>
                                     <form class="user" action="login.php" method="post">
                                         <div class="form-group">
