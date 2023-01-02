c64b647c85c24f732489fe340ab3c9cccd0aeffa
fifiaa543 <118110493+fifiaa543@users.noreply.github.com>
2023-01-02 15:00:34 +0700

    index.php

--- index.php
+++ index.php
@@ -95,7 +95,7 @@ if( isset($_POST['insert']) ){
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fa fa-table" aria-hidden="true"></i>
-                    <span>Data Master</span>
+                    <span>Data Laundry</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
