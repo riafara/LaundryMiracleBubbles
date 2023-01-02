23ca9788ffe0ab0b8d7f127a66fae39b8a7d7fc1
sindisukma <117698581+sindisukma@users.noreply.github.com>
2023-01-02 16:03:44 +0700

    Delete hapusdetailtransaksi.php

--- hapusdetailtransaksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('koneksi.php');
-$id = $_GET['id_detail'];
-mysqli_query($koneksi, "DELETE FROM tb_detail_transaksi WHERE id_detail='$id'") or die(mysql_error());
-header("location: tdttransaksi.php")
-?>
