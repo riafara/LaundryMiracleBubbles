8fa1b18af098c8e4ee77491849896c22e936df35
riafara <riafaradhila30@gmail.com>
2023-01-08 21:45:32 +0700

    dbbaru

--- DB Laundry/laundry.sql
+++ DB Laundry/laundry.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 04, 2023 at 04:56 AM
+-- Generation Time: Jan 08, 2023 at 02:44 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -28,11 +28,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tb_customer` (
-  `kode_customer` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `nama_customer` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `alamat` text COLLATE utf8mb4_general_ci NOT NULL,
-  `nohp` varchar(20) COLLATE utf8mb4_general_ci NOT NULL
+  `kode_customer` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_customer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `alamat` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nohp` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_customer`
+--
+
+INSERT INTO `tb_customer` (`kode_customer`, `nama_customer`, `alamat`, `nohp`) VALUES
+('C001', 'Ria', 'Perum Mastrip', '081234345656'),
+('C002', 'Sindi', 'Perum Tidar', '081212123434'),
+('C003', 'Firda', 'Mastrip IV', '081233443434'),
+('C004', 'Fia', 'Puri Bunga Nirwana', '081233443000');
 
 -- --------------------------------------------------------
 
@@ -41,10 +51,19 @@ CREATE TABLE `tb_customer` (
 --
 
 CREATE TABLE `tb_paket` (
-  `kode_paket` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `kode_paket` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `nama_paket` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `harga_paket` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_paket`
+--
+
+INSERT INTO `tb_paket` (`kode_paket`, `nama_paket`, `harga_paket`) VALUES
+('P001', 'Cuci Setrika', 5000),
+('P002', 'Cuci Kering', 2500),
+('P003', 'Setrika', 2500);
 
 -- --------------------------------------------------------
 
@@ -55,15 +74,25 @@ CREATE TABLE `tb_paket` (
 CREATE TABLE `tb_transaksi` (
   `tgl_masuk` date NOT NULL,
   `kode_transaksi` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `nama_customer` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `nama_paket` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_customer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_paket` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Qty` int NOT NULL,
   `harga_paket` int NOT NULL,
   `harga_total` int NOT NULL,
   `tgl_ambil` date NOT NULL,
-  `status` enum('Selesai','Belum') COLLATE utf8mb4_general_ci NOT NULL,
-  `kode_customer` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `kode_paket` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
+  `status` enum('Selesai','Belum') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `kode_customer` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `kode_paket` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_transaksi`
+--
+
+INSERT INTO `tb_transaksi` (`tgl_masuk`, `kode_transaksi`, `nama_customer`, `nama_paket`, `Qty`, `harga_paket`, `harga_total`, `tgl_ambil`, `status`, `kode_customer`, `kode_paket`) VALUES
+('2023-01-07', 'T001', 'Ria', 'Cuci Setrika', 1, 5000, 5000, '2023-01-09', 'Selesai', 'C001', 'P001'),
+('2023-01-07', 'T002', 'Sindi', 'Cuci Kering', 15, 2500, 37500, '2023-01-09', 'Selesai', 'C002', 'P002'),
+('2023-01-07', 'T003', 'Firda', 'Setrika', 1, 2500, 2500, '2023-01-09', 'Belum', 'C003', 'P003');
 
 -- --------------------------------------------------------
 
@@ -126,7 +155,7 @@ ALTER TABLE `tb_user`
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
